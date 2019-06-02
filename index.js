6331fccc1cd12ba1ab1181e311c8a65d30277d61
garganshul108 <sci.agarg@gmail.com>
2019-06-03 00:31:27 +0530

    mngoDB-local proto2 day 2 check

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// this is the mongo DB branch dated 2 Jun 2019
+// this is the mongo DB branch proto2 dated 2 Jun 2019
 // includes the express function from the express module
 const express = require('express');
 // returns the express function object to app
